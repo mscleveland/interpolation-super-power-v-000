8762f9ed8515ec259568c9df5998202643e62f24
Erin Shank <emshank@gmail.com>
2017-11-30 17:48:32 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
 def display_rainbow(colors)
   puts display_rainbow
-  puts R:{color}, O:{color}, Y:{color}, G:{color}, B:{color}, I:{color}, V:{color}
+  puts R:#{color}, O:#{color}, Y:#{color}, G:#{color}, B:#{color}, I:#{color}, V:#{color}
   color[0], color[1], color[2], color[3], color[4], color[5], color[6]
 end
